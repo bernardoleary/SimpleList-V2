0a369a2d411e1a585eceb66bbbdac05a0ec410cb
Bernard <bernard.oleary@gmail.com>
2012-03-15 09:47:27 +0000

    the user viewmodel, simple enough

---
+++ SimpleList/Infostructure.SimpleList.Web/Models/UserViewModel.cs
@@ -0,0 +1,46 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace Infostructure.SimpleList.Web.Models
+{
+    public class UserViewModel
+    {
+        public int ID
+        {
+            get;
+            set;
+        }
+
+        public string Name
+        {
+            get;
+            set;
+        }
+
+        public string Password
+        {
+            get;
+            set;
+        }
+
+        public string Email
+        {
+            get;
+            set;
+        }
+
+        public bool ShowDoneLists
+        {
+            get;
+            set;
+        }
+
+        public bool ShowDoneListItems
+        {
+            get;
+            set;
+        }
+    }
+}
