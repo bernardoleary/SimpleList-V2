1c5a71634b35a3371751c6ad61f93e4202315891
Bernard <bernard.oleary@gmail.com>
2012-03-15 09:48:50 +0000

    made changes to the user repository, added a couple of views for the user

--- SimpleList/Infostructure.SimpleList.BusinessLogic/Repositories/UserRepository.cs
+++ SimpleList/Infostructure.SimpleList.BusinessLogic/Repositories/UserRepository.cs
@@ -29,48 +29,63 @@ namespace Infostructure.SimpleList.BusinessLogic.Repositories
             return users.FirstOrDefault();
         }
 
-        public void AddUser(User user)
+        public User GetUser(int userId)
         {
-            _simpleListEntities.Users.Add(user);
-            _simpleListEntities.SaveChanges();
+            var users = from user in _simpleListEntities.Users
+                        where user.ID == userId
+                        select user;
+            return users.FirstOrDefault();
         }
 
-        public void UpdatePassword(string userName, string oldPassword, string newPassword)
+        public int AddUser(User user)
+        {
+            _simpleListEntities.Users.Add(user);
+            return _simpleListEntities.SaveChanges();
+        }
+
+        public int  UpdatePassword(string userName, string oldPassword, string newPassword)
         {
             var userForUpdate = GetUser(userName, oldPassword);
             userForUpdate.Password = newPassword;
-            _simpleListEntities.SaveChanges();
+            return _simpleListEntities.SaveChanges();
         }
 
-        public void UpdateEmail(User user, string newEmail)
+        public int UpdateEmail(User user, string newEmail)
         {
             var userForUpdate = GetUser(user.Name, user.Password);
             userForUpdate.Email = newEmail;
-            _simpleListEntities.SaveChanges();
+            return _simpleListEntities.SaveChanges();
         }
 
-        public void UpdateShowDoneLists(string userName, string oldPassword, bool showDoneLists)
+        public int UpdateShowDoneLists(string userName, string oldPassword, bool showDoneLists)
         {
             var userForUpdate = GetUser(userName, oldPassword);
             userForUpdate.ShowDoneLists = showDoneLists;
-            _simpleListEntities.SaveChanges();
+            return _simpleListEntities.SaveChanges();
         }
 
-        public void UpdateShowDoneListItems(string userName, string oldPassword, bool showDoneListItems)
+        public int UpdateShowDoneListItems(string userName, string oldPassword, bool showDoneListItems)
         {
             var userForUpdate = GetUser(userName, oldPassword);
             userForUpdate.ShowDoneListItems = showDoneListItems;
-            _simpleListEntities.SaveChanges();
+            return _simpleListEntities.SaveChanges();
         }
 
-        public void DeleteUser(User user)
+        public int UpdateUser(User user)
+        {
+            var entry = _simpleListEntities.Entry<User>(user);
+            entry.State = System.Data.EntityState.Modified;
+            return _simpleListEntities.SaveChanges();
+        }
+
+        public int DeleteUser(User user)
         {
             var simpleListRepository = new SimpleListRepository(_simpleListEntities);
             var simpleLists = simpleListRepository.GetSimpleLists(user.ID);
             if (simpleLists.Count() > 0)
                 throw new NonEmptyEntityException(NonEmptyEntityException.GetExceptionMessage(typeof(User), typeof(SimpleList.DataModel.Models.SimpleListItem)));
             _simpleListEntities.Users.Remove(user);
-            _simpleListEntities.SaveChanges();
+            return _simpleListEntities.SaveChanges();
         }
     }
 }
---
+++ SimpleList/Infostructure.SimpleList.Web/Views/User/UpdateDetails.aspx
@@ -0,0 +1,77 @@
+﻿<%@ Page Title="" Language="C#" MasterPageFile="~/Views/Shared/Site.Master" Inherits="System.Web.Mvc.ViewPage<Infostructure.SimpleList.Web.Models.UserViewModel>" %>
+
+<asp:Content ID="Content1" ContentPlaceHolderID="TitleContent" runat="server">
+	UpdateDetails
+</asp:Content>
+
+<asp:Content ID="Content2" ContentPlaceHolderID="MainContent" runat="server">
+
+    <h2>UpdateDetails</h2>
+
+    <% using (Html.BeginForm()) {%>
+        <%: Html.ValidationSummary(true) %>
+        
+        <fieldset>
+            <legend>Fields</legend>
+            
+            <div class="editor-label">
+                <%: Html.LabelFor(model => model.ID) %>
+            </div>
+            <div class="editor-field">
+                <%: Html.TextBoxFor(model => model.ID) %>
+                <%: Html.ValidationMessageFor(model => model.ID) %>
+            </div>
+            
+            <div class="editor-label">
+                <%: Html.LabelFor(model => model.Name) %>
+            </div>
+            <div class="editor-field">
+                <%: Html.TextBoxFor(model => model.Name) %>
+                <%: Html.ValidationMessageFor(model => model.Name) %>
+            </div>
+            
+            <div class="editor-label">
+                <%: Html.LabelFor(model => model.Password) %>
+            </div>
+            <div class="editor-field">
+                <%: Html.TextBoxFor(model => model.Password) %>
+                <%: Html.ValidationMessageFor(model => model.Password) %>
+            </div>
+            
+            <div class="editor-label">
+                <%: Html.LabelFor(model => model.Email) %>
+            </div>
+            <div class="editor-field">
+                <%: Html.TextBoxFor(model => model.Email) %>
+                <%: Html.ValidationMessageFor(model => model.Email) %>
+            </div>
+            
+            <div class="editor-label">
+                <%: Html.LabelFor(model => model.ShowDoneLists) %>
+            </div>
+            <div class="editor-field">
+                <%: Html.TextBoxFor(model => model.ShowDoneLists) %>
+                <%: Html.ValidationMessageFor(model => model.ShowDoneLists) %>
+            </div>
+            
+            <div class="editor-label">
+                <%: Html.LabelFor(model => model.ShowDoneListItems) %>
+            </div>
+            <div class="editor-field">
+                <%: Html.TextBoxFor(model => model.ShowDoneListItems) %>
+                <%: Html.ValidationMessageFor(model => model.ShowDoneListItems) %>
+            </div>
+            
+            <p>
+                <input type="submit" value="Save" />
+            </p>
+        </fieldset>
+
+    <% } %>
+
+    <div>
+        <%: Html.ActionLink("Back to List", "Index") %>
+    </div>
+
+</asp:Content>
+
